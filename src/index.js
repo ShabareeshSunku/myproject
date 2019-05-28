7047fbd7ea02007d0ef5e4e622ff66e34eec0835
sabareesh sunku <sabareesh.sunku@espn.com>
2019-05-28 10:58:08 +0530

    controlled and uncontrolled components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import ChatWindow from './Chats/ChatWindow'
 //import Clock from './Clock'
-import VideoPlayer from './VideoPlayer/index'
+//import VideoPlayer from './VideoPlayer/index'
+//import ControlledForm from './Forms/ControlledForm'
+import UnControlledForm from './Forms/UnControlledForm'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<VideoPlayer />, document.getElementById('root'));
+ReactDOM.render(<UnControlledForm />, document.getElementById('root'));
 //ReactDOM.render(<LifeCycleOld />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
